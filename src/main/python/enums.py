1c59b4d2493c3460c8d7857d795ccb13452a7779
coskunyerli <coskun@cavevfx.com>
2020-02-05 14:16:41 +0300

    added product type

--- src/main/python/enums.py
+++ src/main/python/enums.py
@@ -1,3 +1,14 @@
 class BarcodeType:
 	CUSTOM = '0'
 	INVALID = ''
+
+
+class ProductType:
+	REGULAR = 0
+	WEIGHABLE = 1
+
+
+	@classmethod
+	def convertWeighableBarcode(cls, barcode):
+		zero = [0] * len(barcode[2:])
+		return f'{"".join(barcode[0:2])}{"".join(zero)}'
