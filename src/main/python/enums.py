76861ea54fa7db26fedf69a44b78cdc81b89d96a
coskunyerli <coskun@cavevfx.com>
2020-02-07 09:04:05 +0300

    convertWeighableBarcode is refactored

--- src/main/python/enums.py
+++ src/main/python/enums.py
@@ -10,14 +10,16 @@ class ProductType:
 
 	@classmethod
 	def convertWeighableBarcode(cls, barcode):
-		zero = ['0'] * len(barcode[2:5])
-		zero2 = ['0'] * len(barcode[7:])
-		return f'{"".join(barcode[0:2])}{"".join(zero)}{"".join(barcode[5:7])}{"".join(zero2)}'
+		if cls.productType(barcode) == ProductType.WEIGHABLE:
+			zero = ['0'] * len(barcode[7:])
+			return f'{"".join(barcode[0:7])}{"".join(zero)}'
+		else:
+			return barcode
 
 
 	@classmethod
 	def productType(cls, barcode):
-		if len(barcode) > 5 and barcode[0:2] == '27':
+		if len(barcode) > 12 and barcode[0:2] == '27':
 			return cls.WEIGHABLE
 		else:
 			return cls.REGULAR
