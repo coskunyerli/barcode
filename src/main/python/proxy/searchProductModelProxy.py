1e2b1f6db0681ff2fa8d7b4e14c9385bc53ffc33
coskunyerli <coskun.yerli@gmail.com>
2020-02-08 13:21:42 +0300

    add barcode search as temp

--- src/main/python/proxy/searchProductModelProxy.py
+++ src/main/python/proxy/searchProductModelProxy.py
@@ -4,5 +4,8 @@ import PySide2.QtCore as QtCore
 class SearchProductModelProxy(QtCore.QSortFilterProxyModel):
 	def filterAcceptsRow(self, sourceRow, sourceParent):
 		index = self.sourceModel().index(sourceRow, 1, sourceParent)
+		#barcodeIndex = self.sourceModel().index(sourceRow, 0, sourceParent)
 		text = index.data()
-		return self.filterRegExp().pattern().lower() in text.lower()
+		#barcode = barcodeIndex.data()
+		return (self.filterRegExp().pattern().lower() in text.lower())
+	#barcode.startswith(self.filterRegExp().pattern().lower())
