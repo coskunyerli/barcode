58d22b9bdc45aec5ccda8f18245de4af836ac095
coskunyerli <coskun.yerli@gmail.com>
2020-02-08 21:25:56 +0300

    defined qss method

--- src/main/python/fbs.py
+++ src/main/python/fbs.py
@@ -1,3 +1,5 @@
+import os
+
 from fbs_runtime.application_context import cached_property
 from fbs_runtime.application_context.PySide2 import ApplicationContext
 
@@ -9,3 +11,12 @@ class BarcodeApplicationContext(ApplicationContext):
 		result.setApplicationName(self.build_settings['app_name'])
 		result.setApplicationVersion(self.build_settings['version'])
 		return result
+
+
+	def qss(self, filename):
+		try:
+			with open(self.get_resource(os.path.join('qss', filename))) as file:
+				qssString = file.read()
+				return qssString
+		except Exception as e:
+			return None
