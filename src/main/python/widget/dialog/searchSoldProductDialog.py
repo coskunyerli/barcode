a4e14981326f31d2e1a8ea020e432eca78b4f2a1
coskunyerli <coskun.yerli@gmail.com>
2020-03-22 13:28:06 +0300

    add start and stop time

--- src/main/python/widget/dialog/searchSoldProductDialog.py
+++ src/main/python/widget/dialog/searchSoldProductDialog.py
@@ -53,12 +53,20 @@ class SearchSoldProductDialog(QtWidgets.QDialog, DatabaseService):
 		self.lineEdit.setPlaceholderText('Enter a product name to search')
 
 		self.lineEdit.setStyleSheet('padding-left:8px;border-radius:4px;color:white')
-		self.calenderLineEdit = QtWidgets.QDateTimeEdit(QtCore.QDate(QtCore.QDate.currentDate()), self.searchWidget)
-		self.calenderLineEdit.setCalendarPopup(True)
-		self.calenderLineEdit.setStyleSheet('color:black')
+		self.startCheckBox = QtWidgets.QCheckBox(self.searchWidget)
+		self.startCheckBox.setText('Disable Start Time')
+		self.startCalenderLineEdit = QtWidgets.QDateTimeEdit(QtCore.QDate(QtCore.QDate.currentDate()),
+															 self.searchWidget)
+		self.startCalenderLineEdit.setCalendarPopup(True)
+		self.startCalenderLineEdit.setStyleSheet('color:black')
+
+		self.stopCheckBox = QtWidgets.QCheckBox(self.searchWidget)
+		self.stopCheckBox.setText('Disable Stop Time')
+		self.stopCalenderLineEdit = QtWidgets.QDateTimeEdit(QtCore.QDate(QtCore.QDate.currentDate()), self.searchWidget)
+		self.stopCalenderLineEdit.setCalendarPopup(True)
+		self.stopCalenderLineEdit.setStyleSheet('color:black')
 
 		self.limitWidget = QtWidgets.QWidget(self.searchWidget)
-		# self.limitWidget.setSizePolicy(sizePolicy)
 		self.limitWidgetLayout = QtWidgets.QHBoxLayout(self.limitWidget)
 		self.limitWidgetLayout.setContentsMargins(0, 0, 0, 0)
 		self.limitWidgetLayout.setSpacing(0)
@@ -78,7 +86,10 @@ class SearchSoldProductDialog(QtWidgets.QDialog, DatabaseService):
 		self.searchButton.setText('Search')
 
 		self.searchWidgetLayout.addWidget(self.lineEdit)
-		self.searchWidgetLayout.addWidget(self.calenderLineEdit)
+		self.searchWidgetLayout.addWidget(self.startCheckBox)
+		self.searchWidgetLayout.addWidget(self.startCalenderLineEdit)
+		self.searchWidgetLayout.addWidget(self.stopCheckBox)
+		self.searchWidgetLayout.addWidget(self.stopCalenderLineEdit)
 		self.searchWidgetLayout.addWidget(self.limitWidget)
 		self.searchWidgetLayout.addWidget(self.searchButton)
 
@@ -91,6 +102,10 @@ class SearchSoldProductDialog(QtWidgets.QDialog, DatabaseService):
 
 		self.orderModel = OrderModel()
 		self.listView.setModel(self.orderModel)
+		self.startCheckBox.stateChanged.connect(self.__updateStartCalender)
+		self.stopCheckBox.stateChanged.connect(self.__updateStopCalender)
+
+		self.startCheckBox.setChecked(True)
 
 		self.__search()
 
@@ -113,17 +128,31 @@ class SearchSoldProductDialog(QtWidgets.QDialog, DatabaseService):
 		SearchSoldProductDialog.sizeInfo().headerSizes = headerSizes
 
 
+	def __updateStartCalender(self, value):
+		self.startCalenderLineEdit.setDisabled(value)
+
+
+	def __updateStopCalender(self, value):
+		self.stopCalenderLineEdit.setDisabled(value)
+
+
 	def __search(self):
 		nameText = self.lineEdit.text()
-		date = self.calenderLineEdit.date().toPython()
-		dtime = datetime.datetime.combine(date, datetime.datetime.max.time())
+		sdate = self.startCalenderLineEdit.date().toPython()
+		startDate = datetime.datetime.combine(sdate, datetime.datetime.min.time())
+
+		eDate = self.stopCalenderLineEdit.date().toPython()
+		stopDate = datetime.datetime.combine(eDate, datetime.datetime.max.time())
 
 		limit = self.limitSpinbox.value()
 		query = self.databaseService().query(Order)
 		if nameText:
 			query = query.join(DatabaseSoldProduct).filter(
 					DatabaseSoldProduct.product_name.like(f'%{nameText}%'))
-		query = query.filter(DatabaseOrder.created_date <= dtime)
+		if self.stopCalenderLineEdit.isEnabled():
+			query = query.filter(DatabaseOrder.created_date <= stopDate)
+		if self.startCalenderLineEdit.isEnabled():
+			query = query.filter(startDate <= DatabaseOrder.created_date)
 		query = query.order_by(desc(DatabaseOrder.created_date)).limit(limit)
 
 		orderList = []
